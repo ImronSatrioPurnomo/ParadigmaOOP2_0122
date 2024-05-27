abc983c49dc2a10aea2eb98c2967e71f1333c18e
ImronSatrioPurnomo <satriogames2223@gmail.com>
2024-05-27 10:47:29 +0700

    add class budi

--- MultiWaris/MultiWaris.cpp
+++ MultiWaris/MultiWaris.cpp
@@ -27,3 +27,13 @@ class pelajar : public orang {
 		cout << "pelajar dibuat\n" << endl;
 	}
 };
+
+class budi : public pekerja, public pelajar {
+public:
+	budi(int pUmur) :
+		pekerja(pUmur),
+		pelajar(pUmur)
+	{
+		cout << "budi dibuat\n" << endl;
+	}
+};
